881c3cf731c836070858baa2dd6daed891a09815
chonker <36255253+Derro8@users.noreply.github.com>
2025-05-25 18:58:11 -0700

    Update dump_basic to output the token

--- Tools/dump_basic.py
+++ Tools/dump_basic.py
@@ -5,6 +5,9 @@ import re
 import base64
 import os
 import json
+import sys
+
+args = sys.argv[1:]
 
 player_bundle = "https://static.crunchyroll.com/vilos-v2/web/vilos/js/bundle.js"
 
@@ -22,29 +25,36 @@ def get_b64(string: str) -> str:
     return base64.b64encode(string.encode("iso-8859-1")).decode()
 
 if __name__ == "__main__":
+    prod_user, prod_pass = prod.split(":")
     path = os.path.join(os.path.dirname(__file__), "basic.json")
-    with open(path, "w") as f:
-        prod_user, prod_pass = prod.split(":")
-        staging_user, staging_pass = staging.split(":")
-        proto_user, proto_pass = proto.split(":")
-    
-        f.write(json.dumps({
-            "production": {
-                "token": get_b64(prod),
-                "username": prod_user,
-                "password": prod_pass
-            },
-            "staging": {
-                "token": get_b64(staging),
-                "username": staging_user,
-                "password": staging_pass
-            },
-            "proto": {
-                "token": get_b64(proto),
-                "username": proto_user,
-                "password": proto_pass
-            }
-        }, indent=4))
-    
-    print(f"Dumped tokens to {path}")
+    prod_token = get_b64(prod)
+
+    if len(args) > 0 and args[0] == "update":
+        with open(path, "w") as f:
+            staging_user, staging_pass = staging.split(":")
+            proto_user, proto_pass = proto.split(":")
+
+                
+            f.write(json.dumps({
+                "production": {
+                    "token": prod_token,
+                    "username": prod_user,
+                    "password": prod_pass
+                },
+                "staging": {
+                    "token": get_b64(staging),
+                    "username": staging_user,
+                    "password": staging_pass
+                },
+                "proto": {
+                    "token": get_b64(proto),
+                    "username": proto_user,
+                    "password": proto_pass
+                }
+            }, indent=4))
+
+        print(f"Dumped tokens to {path}")
+    else:
+        print("Basic Auth Token:")
+        print(f"{prod_token}")
 
